7cb566de4e2e6f6fe24e9407f7d930e2853863de
Shivam Pandey <pandeyshivam1312@gmail.com>
2022-01-06 10:50:38 +0530

    feat: Enter key search

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,11 +1,20 @@
 
 export default function Search({ setSearch, onClick}){
+
+    
+
     return (
         <div className="search__component">
             <input
             type = "text"
             placeholder="Enter Username"
             onChange={(e) => setSearch(e.target.value)}
+            onKeyDown={(e) => {
+                if(e.key === 'Enter'){
+                    onClick();
+                    
+                }
+            }}
             />
             <button onClick={()=>onClick()}>Add</button>
         </div>
